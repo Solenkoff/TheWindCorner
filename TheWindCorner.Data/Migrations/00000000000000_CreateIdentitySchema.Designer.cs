711c68e93e7cec7c31e5322788b4e96cf0549e9b
Solenkoff <solenkoff@gmail.com>
2025-07-23 14:52:08 +0300

    Move Identity and Migrations to Data layer and resolve conflicts

--- TheWindCorner.Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ TheWindCorner.Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TheWindCorner.Data;
 using TheWindCorner.Web.Data;
 
 namespace TheWindCorner.Web.Data.Migrations
