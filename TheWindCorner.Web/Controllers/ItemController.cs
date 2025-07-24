83fdbe772a06da8dd07f740b43eea27efae21a12
Solenkoff <solenkoff@gmail.com>
2025-07-24 12:35:49 +0300

    Add ItemController with Index Action as well as Index View and link for it in the NavBar

---
+++ TheWindCorner.Web/Controllers/ItemController.cs
@@ -0,0 +1,28 @@
+﻿namespace TheWindCorner.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using TheWindCorner.Data;
+    using TheWindCorner.Data.Models.Entities;
+    using TheWindCorner.Data.Models.Enums;
+    using TheWindCorner.Web.ViewModels.Item;
+
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ItemController(ApplicationDbContext _dbContext)
+        {
+            this.dbContext = _dbContext;          
+        }
+
+        [HttpGet]    
+        public IActionResult Index()
+        {
+            IEnumerable<Item> allItems = this.dbContext
+                .Items
+                .ToList();
+            return View(allItems);
+        }
+    }
+}
