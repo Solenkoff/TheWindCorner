d7cc4a608e9448cdf22c14c0f85ccfed084c9bfe
Solenkoff <solenkoff@gmail.com>
2025-07-24 11:49:46 +0300

    Add AddItemInputModel with some additional validation attributes

--- TheWindCorner.Common/EntityValidationConstants.cs
+++ TheWindCorner.Common/EntityValidationConstants.cs
@@ -6,10 +6,14 @@
 
         public static class Item
         {
+            public const int ItemTitleMinLength = 3;
             public const int ItemTitleMaxLength = 80;
+            public const int ItemSizeMinLength = 2;
             public const int ItemSizeMaxLength = 50;
+            public const int ItemBrandMinLength = 3;
             public const int ItemBrandMaxLength = 80;
             public const int ItemMinYear = 2000;
+            public const int ItemDescriptionMinLength = 10;
             public const int ItemDescriptionMaxLength = 1000;
 
         }
