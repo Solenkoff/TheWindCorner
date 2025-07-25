df139fee64b7d3c7ec5d19e83b1a5db453a20516
Solenkoff <solenkoff@gmail.com>
2025-07-25 14:27:25 +0300

    Add DummyEmailSender to infrastructure

---
+++ TheWindCorner.Web.Infrastructure/Email/DummyEmailSender.cs
@@ -0,0 +1,14 @@
+﻿namespace TheWindCorner.Web.Infrastructure.Email
+{
+    using Microsoft.AspNetCore.Identity.UI.Services;
+
+    public class DummyEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            Console.WriteLine($"[Email to {email}] {subject}: {htmlMessage}");
+            return Task.CompletedTask;
+        }
+    }
+  
+}
