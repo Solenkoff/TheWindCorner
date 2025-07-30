c8b4c33b95ce824174026f8cf09509534bb9755c
Solenkoff <solenkoff@gmail.com>
2025-07-30 11:16:00 +0300

    Add entity WantedItemComment

---
+++ TheWindCorner.Data.Models/Entities/WantedItemComment.cs
@@ -0,0 +1,51 @@
+﻿namespace TheWindCorner.Data.Models.Entities
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using TheWindCorner.Data.Models.User;
+
+    using static TheWindCorner.Common.EntityValidationConstants.WantedItemComment;
+    using static TheWindCorner.Common.EntityValidationMessages.WantedItemComment;
+
+
+    [Comment("The comment on an wanted item")]
+    public class WantedItemComment
+    {
+
+        [Key]
+        [Comment("The identifier of the comment made")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(ContentMaxLength, ErrorMessage = ContentMaxLengthMassage)]
+        [Comment("The full text of the comment")]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        [Comment("The date and time the comment was added or edited")]
+        public DateTime CommentedOn { get; set; }
+
+        [Required]
+        [Comment("If the comment has been deleted")]
+        public bool IsDeleted { get; set; } = false;
+
+        [Required]
+        [Comment("The identifier of the user, who made the comment")]
+        public Guid OwnerId { get; set; }
+
+        [ForeignKey(nameof(OwnerId))]
+        [Comment("The user, who made the comment")]
+        public virtual ApplicationUser Owner { get; set; } = null!;
+
+        [Required]
+        [Comment("The identifier of the item, that was commented")]
+        public Guid WantedItemId { get; set; }
+
+        [ForeignKey(nameof(WantedItemId))]
+        [Comment("The item, that was commented")]
+        public virtual WantedItem WantedItem { get; set; } = null!;
+
+    }
+}
