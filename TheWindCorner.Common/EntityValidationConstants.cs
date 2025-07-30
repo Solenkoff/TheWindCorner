afb20f03a4f91ee93459133a9174673defbeb105
Solenkoff <solenkoff@gmail.com>
2025-07-30 17:13:08 +0300

    Extend EntityValidationConstants and EntityValidationMessages

--- TheWindCorner.Common/EntityValidationConstants.cs
+++ TheWindCorner.Common/EntityValidationConstants.cs
@@ -3,26 +3,117 @@
        
     public static class EntityValidationConstants
     {
-
         public static class Item
         {
-            public const int ItemTitleMinLength = 3;
-            public const int ItemTitleMaxLength = 80;
-            public const int ItemSizeMinLength = 2;
-            public const int ItemSizeMaxLength = 50;
-            public const int ItemBrandMinLength = 3;
-            public const int ItemBrandMaxLength = 80;
-            public const int ItemMinYear = 2000;
-            public const int ItemDescriptionMinLength = 10;
-            public const int ItemDescriptionMaxLength = 1000;
+            public const int TitleMinLength = 3;
+            public const int TitleMaxLength = 80; 
 
+            public const int SizeMinLength = 2;
+            public const int SizeMaxLength = 50;
+
+            public const int BrandMinLength = 3;
+            public const int BrandMaxLength = 80;
+
+            public const int ModelMinLength = 2;
+            public const int ModelMaxLength = 80;
+
+            public const int MinYear = 2000;
+
+            public const string PriceMinValue = "1";
+            public const string PriceMaxValue = "10000";
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 1000;
+
+        }
+
+        public static class WantedItem
+        {
+            public const int TitleMinLength = 3;
+            public const int TitleMaxLength = 80;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 1000;
         }
 
         public static class ItemComment
         {
+            public const int ContentMinLength = 2;
             public const int ContentMaxLength = 500;
         }
 
+        public static class WantedItemComment
+        {
+            public const int ContentMinLength = 2;
+            public const int ContentMaxLength = 500;
+        }
 
+        public static class SpotComment
+        {
+            public const int ContentMinLength = 2;
+            public const int ContentMaxLength = 1000;
+        }
+
+        public static class User
+        {
+            public const int UsernameMinLength = 3;
+            public const int UsernameMaxLength = 20;
+
+            public const int FirstNameMinLength = 2;
+            public const int FirstNameMaxLength = 50;
+
+            public const int LastNameMinLength = 2;
+            public const int LastNameMaxLength = 50;
+
+            public const int PhoneNumberMinLength = 7;
+            public const int PhoneNumberMaxLength = 15;
+
+            public const int PasswordMinLength = 6;
+            public const int PasswordMaxLength = 64;
+
+            public const int EmailMinLength = 5;
+            public const int EmailMaxLength = 320;
+        }
+
+        public static class Image
+        {
+            public const int TitleMinLength = 3;
+            public const int TitleMaxLength = 100;
+
+            public const int TextMinLength = 10;
+            public const int TextMaxLength = 1000;
+        }
+
+        public static class Spot 
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 100;
+
+            public const int DescriptionMinLength = 3;
+            public const int DescriptionMaxLength = 1000;
+
+            public const double MinLatitude = -90;
+            public const double MaxLatitude = 90;
+
+            public const double MinLongitude = -180;
+            public const double MaxLongitude = 180;
+        }
+
+        public static class Event
+        {
+            public const int TitleMinLength = 2;
+            public const int TitleMaxLength = 50;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 500;
+
+            public const int FullInfoMinLength = 10;
+            public const int FullInfoMaxLength = 2000;
+
+            public const int LocationMinLength = 2;
+            public const int LocationMaxLength = 100;
+        }
+
+        
     }
 }
--- TheWindCorner.Common/EntityValidationMessages.cs
+++ TheWindCorner.Common/EntityValidationMessages.cs
@@ -6,22 +6,84 @@
 
         public static class Item
         {
-            public const string ItemYearValidationMassage = "Please enter a valid year, 2000 or later!";
-            public const string ItemCategoryValidationMassage = "Invalid item category!";
-            public const string ItemTypeValidationMassage = "Invalid item type!";
-            public const string ItemTitleMinLengthMassage = "Item Title should be at least 3 characters long!";
-            public const string ItemTitleMaxLengthMassage = "Item Title should not be more than 80 characters long!";
-            public const string ItemSizeMinLengthMassage = "Item Size should be at least 2 characters long!";
-            public const string ItemSizeMaxLengthMassage = "Item Size should not be more than 50 characters long!";
-            public const string ItemBrandMinLengthMassage = "Item Brand should be at least 3 characters long!";
-            public const string ItemBrandMaxLengthMassage = "Item Brand should not be more than 80 characters long!";
-            public const string ItemDescriptionMinLengthMassage = "Item Description should be at least 10 characters long!";
-            public const string ItemDescriptionMaxLengthMassage = "Item Description should not be more than 1000 characters long!";
+            public const string YearValidationMassage = "Please enter a valid year, 2000 or later!";
+            public const string CategoryValidationMassage = "Invalid item category!";
+            public const string TypeValidationMassage = "Invalid item type!";
+            public const string TitleMinLengthMassage = "Item Title should be at least 3 characters long!";
+            public const string TitleMaxLengthMassage = "Item Title should not be more than 80 characters long!";
+            public const string SizeMinLengthMassage = "Item Size should be at least 2 characters long!";
+            public const string SizeMaxLengthMassage = "Item Size should not be more than 50 characters long!";
+            public const string BrandMinLengthMassage = "Item Brand should be at least 3 characters long!";
+            public const string BrandMaxLengthMassage = "Item Brand should not be more than 80 characters long!";
+            public const string DescriptionMinLengthMassage = "Item Description should be at least 10 characters long!";
+            public const string DescriptionMaxLengthMassage = "Item Description should not be more than 1000 characters long!";
         }
 
         public static class ItemComment
         {
+            public const string ContentMinLengthMassage = "Item Comment should be at least 2 characters long!";
             public const string ContentMaxLengthMassage = "Item Comment should not be more than 500 characters long!";
         }
+
+        public static class WantedItemComment
+        {
+            public const string ContentMinLengthMassage = "Wanted item comment should be at least 2 characters long!";
+            public const string ContentMaxLengthMassage = "Wanted item comment should not be more than 500 characters long!";
+        }
+
+        public static class Spot
+        {
+            public const string NameMinLengthMessage = "Name should be at least 3 characters long!";
+            public const string NameMaxLengthMessage = "Name should not be more than 100 characters long!";
+
+            public const string DescriptionMinLengthMessage = "Description should be at least 3 characters long!";
+            public const string DescriptionMaxLengthMessage = "Description should not be more than 1000 characters long!";
+
+            public const string LatitudeValidationMessage = "Latitude must be between -90 and 90 degrees.";
+            public const string LongitudeValidationMessage = "Longitude must be between -180 and 180 degrees.";
+        }
+
+        public static class SpotComment
+        {
+            public const string ContentMinLengthMassage = "Item Comment should be at least 2 characters long!";
+            public const string ContentMaxLengthMassage = "Item Comment should not be more than 1000 characters long!";
+        }
+
+        public static class User
+        {
+            public const string UsernameMinLengthMessage = "Item Title should be at least 3 characters long!";
+            public const string UsernameMaxLengthMessage = "Wanted item comment should not be more than 500 characters long!";
+
+            public const string FirstNameMinLengthMessage = "Item Title should be at least 2 characters long!";
+            public const string FirstNameMaxLengthMessage = "Wanted item comment should not be more than 50 characters long!";
+
+            public const string LastNameMinLengthMessage = "Item Title should be at least 2 characters long!";
+            public const string LastNameMaxLengthMessage = "Wanted item comment should not be more than 50 characters long!";
+
+            public const string PhoneNumberMinLengthMessage = "Phone number should be at least 7 characters long!";
+            public const string PhoneNumberMaxLengthMessage = "Phone number should not be more than 15 characters long!";
+
+            public const string PasswordMinLengthMessage = "Password should be at least 6 characters long!";
+            public const string PasswordMaxLengthMessage = "Password should not be more than 64 characters long!";
+
+            public const string EmailMinLengthMessage = "Email should be at least 5 characters long!";
+            public const string EmailMaxLengthMessage = "Email should not be more than 320 characters long!";
+        }
+
+        public static class Event
+        {
+            public const string TitleMinLengthMassage = "Title should be at least 2 characters long!";
+            public const string TitleMaxLengthMassage = "Title should not be more than 50 characters long!";
+
+            public const string DescriptionMinLengthMassage = "Description should be at least 10 characters long!";
+            public const string DescriptionMaxLengthMassage = "Description should not be more than 500 characters long!";
+
+            public const string FullInfoMinLengthMassage = "FullInfo should be at least 10 characters long!";
+            public const string FullInfoMaxLengthMassage = "FullInfo should not be more than 2000 characters long!";
+
+            public const string LocationMinLengthMassage = "Location should be at least 2 characters long!";
+            public const string LocationMaxLengthMassage = "Location should not be more than 100 characters long!";
+        }
+
     }
 }
