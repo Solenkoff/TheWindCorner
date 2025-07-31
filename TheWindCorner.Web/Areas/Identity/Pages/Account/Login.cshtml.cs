f26a93f1d6db8b0ed8d4f81a2b1f4857ca9676c1
Solenkoff <solenkoff@gmail.com>
2025-07-31 17:26:26 +0300

    Clean or comment code for future use in Login

--- TheWindCorner.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TheWindCorner.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,22 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using TheWindCorner.Data.Models.User;
-
 namespace TheWindCorner.Web.Areas.Identity.Pages.Account
 {
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using TheWindCorner.Data.Models.User;
+
     public class LoginModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
@@ -40,7 +33,7 @@ namespace TheWindCorner.Web.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+        //  !!!  public IList<AuthenticationScheme> ExternalLogins { get; set; }       //  !!! 
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -97,7 +90,7 @@ namespace TheWindCorner.Web.Areas.Identity.Pages.Account
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            //  !!! ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();     //  !!! 
 
             ReturnUrl = returnUrl;
         }
@@ -106,7 +99,7 @@ namespace TheWindCorner.Web.Areas.Identity.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            //  !!! ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();     //  !!! 
 
             if (ModelState.IsValid)
             {
