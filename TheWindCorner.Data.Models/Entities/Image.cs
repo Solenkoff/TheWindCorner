cc00708a864059dbaf169538e206af6646946b57
Solenkoff <solenkoff@gmail.com>
2025-08-02 13:20:22 +0300

    Expand Image Entity

--- TheWindCorner.Data.Models/Entities/Image.cs
+++ TheWindCorner.Data.Models/Entities/Image.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.EntityFrameworkCore;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     using static TheWindCorner.Common.EntityValidationConstants.Image;
 
@@ -23,6 +24,40 @@
         [Required]
         [Comment("The path to the image")]
         public string Path { get; set; } = null!;
+
+
+        [Comment("The identifier of the related item")]
+        public Guid? ItemId { get; set; }
+
+        [ForeignKey(nameof(ItemId))]
+        [Comment("The related item")]
+        public virtual Item? Item { get; set; }
+
+
+        [Comment("The identifier of the related wanted item")]
+        public Guid? WantedItemId { get; set; }
+
+        [ForeignKey(nameof(WantedItemId))]
+        [Comment("The related wanted item")]
+        public virtual WantedItem? WantedItem { get; set; }
+
+
+        [Comment("The identifier of the related spot")]
+        public Guid? SpotId { get; set; }
+
+        [ForeignKey(nameof(SpotId))]
+        [Comment("The related spot")]
+        public virtual Spot? Spot { get; set; }
+
+
+        [Comment("The identifier of the related event")]
+        public Guid? EventId { get; set; }
+
+        [ForeignKey(nameof(EventId))]
+        [Comment("The related event")]
+        public virtual Event? Event { get; set; }
+
+
 
     }
 }
