c29f03a10e9956fb4ce91dac1c3b8dffc8af7a62
Solenkoff <solenkoff@gmail.com>
2025-08-03 14:02:31 +0300

    Add EventConfiguration

---
+++ TheWindCorner.Data/Configurations/EventConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using TheWindCorner.Data.Models.Entities;
+    using TheWindCorner.Data.Models.Entities.Enums;
+
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder
+                .HasKey(e => e.Id);
+
+            builder.Property(e => e.Status)
+                .IsRequired()
+                .HasDefaultValue(EventStatus.Upcoming);
+
+        }
+    }
+}
