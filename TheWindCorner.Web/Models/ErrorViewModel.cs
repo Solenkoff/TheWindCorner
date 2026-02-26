d9d43efdb3d09a1319bbeb86150925e9631c946d
Solenkoff <solenkoff@yahoo.com>
2026-02-26 17:22:43 +0200

    Initialize  project

---
+++ TheWindCorner.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheWindCorner.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
