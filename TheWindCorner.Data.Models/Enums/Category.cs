64b12da8c0bb44cf8aa5f4012000b821166a2048
Solenkoff <solenkoff@yahoo.com>
2026-02-28 11:56:16 +0200

    Alter Category and ItemType enums

--- TheWindCorner.Data.Models/Enums/Category.cs
+++ TheWindCorner.Data.Models/Enums/Category.cs
@@ -1,12 +1,9 @@
 ﻿namespace TheWindCorner.Data.Models.Enums
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    internal class Category
+    public enum Category
     {
+        Windsurfing = 0,
+        Kitesurfing = 1,
+        Wingfoiling = 2
     }
 }
