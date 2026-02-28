72c00cbac39e8b74959bdc535f116a64536c466b
Solenkoff <solenkoff@yahoo.com>
2026-02-28 14:59:01 +0200

    Add Item entity

---
+++ TheWindCorner.Data.Models/Entities/Item.cs
@@ -0,0 +1,58 @@
+﻿namespace TheWindCorner.Data.Models.Entities
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+
+    using TheWindCorner.Data.Models.Enums;
+
+    using static TheWindCorner.Common.EntityValidationConstants.Item;
+
+    public class Item
+    {
+        [Key]
+        [Comment("Item Identifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Comment("The Category of the Item")]
+        public Category Category { get; set; }
+
+        [Required]
+        [Comment("The Type of the Item")]
+        public ItemType ItemType { get; set; }
+
+        [Required]
+        [MaxLength(ItemTitleMaxLength)]
+        [Comment("A short descriptive title for the item")]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(ItemSizeMaxLength)]
+        [Comment("The Size of the Item")]
+        public string? Size { get; set; }
+
+        [MaxLength(ItemBrandMaxLength)]
+        [Comment("The Brand Name of the Item")]
+        public string? Brand { get; set; }
+
+        [Comment("The Year of the Item's production or collection")]
+        public int Year { get; set; }
+
+        [Required]
+        [MaxLength(ItemDescriptionMaxLength)]
+        [Comment("A full description of the item")]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Comment("If the Item has been approved for listing")]
+        public bool IsApproved { get; set; } = false;
+
+        [Required]
+        [Comment("If the Item has been deleted")]
+        public bool IsDeleted { get; set; } = false;
+
+        [Required]
+        [Comment("If the Item has been sold")]
+        public bool IsSold { get; set; } = false;
+    }
+}
