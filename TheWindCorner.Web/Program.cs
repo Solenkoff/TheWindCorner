07ec7236014236ca9823e33c128415f045168c14
Solenkoff <solenkoff@yahoo.com>
2026-03-01 10:48:22 +0200

    Set Connection String to Secrets and fix appsettings

--- TheWindCorner.Web/Program.cs
+++ TheWindCorner.Web/Program.cs
@@ -11,9 +11,12 @@ namespace TheWindCorner.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+                                         throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
