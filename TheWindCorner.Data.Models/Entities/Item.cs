112843254326a5f7ae52e9122bd4c204cb854d22
Solenkoff <solenkoff@yahoo.com>
2026-03-01 13:20:42 +0200

    Updated Item entity and included some missed commits

--- TheWindCorner.Data.Models/Entities/Item.cs
+++ TheWindCorner.Data.Models/Entities/Item.cs
@@ -1,18 +1,20 @@
 ﻿namespace TheWindCorner.Data.Models.Entities
 {
     using System;
-    using Microsoft.EntityFrameworkCore;
     using System.ComponentModel.DataAnnotations;
+    using Microsoft.EntityFrameworkCore;
 
+    using TheWindCorner.Web.Infrastructure.Attributes;
     using TheWindCorner.Data.Models.Enums;
 
     using static TheWindCorner.Common.EntityValidationConstants.Item;
+    using static TheWindCorner.Common.EntityValidationMessages.Item;
 
     public class Item
     {
         [Key]
         [Comment("Item Identifier")]
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
 
         [Required]
         [Comment("The Category of the Item")]
@@ -23,37 +25,54 @@
         public ItemType ItemType { get; set; }
 
         [Required]
-        [MaxLength(ItemTitleMaxLength)]
+        [MaxLength(TitleMaxLength)]
         [Comment("A short descriptive title for the item")]
         public string Title { get; set; } = null!;
 
-        [MaxLength(ItemSizeMaxLength)]
+        [MaxLength(SizeMaxLength)]
         [Comment("The Size of the Item")]
         public string? Size { get; set; }
 
-        [MaxLength(ItemBrandMaxLength)]
+        [MaxLength(BrandMaxLength)]
         [Comment("The Brand Name of the Item")]
         public string? Brand { get; set; }
 
-        [RangeUntilCurrentYear(ItemMinYear, ErrorMessage = "Please enter a valid year, 2000 or later!")]
+        [MaxLength(ModelMaxLength)]
+        [Comment("The Model Name of the Item")]
+        public string? Model { get; set; }
+
+
+        [RangeUntilCurrentYear(MinYear, ErrorMessage = YearValidationMassage)]
         [Comment("The Year of the Item's production or collection")]
         public int Year { get; set; }
 
         [Required]
-        [MaxLength(ItemDescriptionMaxLength)]
+        [Precision(18, 2)]
+        [Range(typeof(decimal), PriceMinValue, PriceMaxValue)]
+        [Comment("The Price of the Item")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
         [Comment("A full description of the item")]
         public string Description { get; set; } = null!;
+
+        [Required]
+        [Comment("The date and time, when the item was listed for sale")]
+        public DateTime DateAdded { get; set; }
 
         [Required]
         [Comment("If the Item has been approved for listing")]
         public bool IsApproved { get; set; } = false;
 
         [Required]
+        [Comment("If the Item has been sold")]
+        public bool IsSold { get; set; } = false;
+
+        [Required]
         [Comment("If the Item has been deleted")]
         public bool IsDeleted { get; set; } = false;
 
-        [Required]
-        [Comment("If the Item has been sold")]
-        public bool IsSold { get; set; } = false;
+       
     }
 }
