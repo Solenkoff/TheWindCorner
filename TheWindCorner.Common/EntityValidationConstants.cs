dada9c4b1b9e1f48274e9c863844f701d00aa9f9
Solenkoff <solenkoff@yahoo.com>
2026-03-01 12:34:21 +0200

    Added EntityValidationMessages  and updated EntityValidationConstants

--- TheWindCorner.Common/EntityValidationConstants.cs
+++ TheWindCorner.Common/EntityValidationConstants.cs
@@ -5,11 +5,14 @@
 
         public static class Item
         {
-            public const int ItemTitleMaxLength = 80;
-            public const int ItemSizeMaxLength = 50;
-            public const int ItemBrandMaxLength = 80;
-            public const int ItemMinYear = 2000;
-            public const int ItemDescriptionMaxLength = 1000;
+            public const int TitleMaxLength = 80;
+            public const int SizeMaxLength = 50;
+            public const int BrandMaxLength = 80;
+            public const int ModelMaxLength = 80;
+            public const int MinYear = 2000;
+            public const string PriceMinValue = "1";
+            public const string PriceMaxValue = "10000";
+            public const int DescriptionMaxLength = 1000;
 
         }
 
---
+++ TheWindCorner.Common/EntityValidationMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace TheWindCorner.Common
+{
+
+    public static class EntityValidationMessages
+    {
+        public static class Item
+        {
+            public const string YearValidationMassage = "Please enter a valid year, 2000 or later!";
+        }
+
+
+
+    }
+}
