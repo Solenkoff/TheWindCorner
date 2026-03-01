cc35a525ada8c9ea3e0b13393eb068c3f6b8c873
Solenkoff <solenkoff@yahoo.com>
2026-03-01 11:42:38 +0200

    Added more NotifiableEntityTypes and INotifiableEntity interface

---
+++ TheWindCorner.Data.Models/Entities/Contracts/INotifiableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace TheWindCorner.Data.Models.Entities.Contracts
+{
+    using System;
+    using TheWindCorner.Data.Models.Enums;
+
+    public interface INotifiableEntity
+    {
+        Guid Id { get; }
+        Guid OwnerId { get; }
+        NotifiableEntityType EntityType { get; }
+    }
+}
---
+++ TheWindCorner.Data.Models/Enums/NotifiableEntityType.cs
@@ -0,0 +1,14 @@
+﻿namespace TheWindCorner.Data.Models.Enums
+{
+    public enum NotifiableEntityType
+    {
+        Unknown = 0,
+        Item = 1,
+        WantedItem = 2,
+        Spot = 3,
+        ItemComment = 4,
+        WantedItemComment = 5,
+        SpotComment = 6,
+        EventComment = 7
+    }
+}
