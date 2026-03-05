da8fb47192e065184d0f7d3ad4d9c1cbaa57959f
Solenkoff <solenkoff@yahoo.com>
2026-03-05 15:48:03 +0200

    Add Notificatioin entity

---
+++ TheWindCorner.Data.Models/Entities/Notification.cs
@@ -0,0 +1,48 @@
+﻿namespace TheWindCorner.Data.Models.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Microsoft.EntityFrameworkCore;
+
+    using TheWindCorner.Data.Models.Enums;
+    using TheWindCorner.Data.Models.User;
+
+
+    [Comment("Notification to the admin about user's action")]
+    public class Notification
+    {
+
+        [Key]
+        [Comment("The identifier of the notification")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [Comment("The date and time the notification was sent")]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        [Comment("The identifier of the entity that triggered the notification")]
+        public Guid EntityId { get; set; }
+
+        [Required]
+        [Comment("The type of the entity")]
+        public NotifiableEntityType EntityType { get; set; }
+
+        [Required]
+        [Comment("If the notification has been read by the admin")]
+        public bool IsRead { get; set; } = false;
+
+        [Required]
+        [Comment("If the notified action has been handled by the admin")]
+        public bool IsHandled { get; set; } = false;
+
+        [Required]
+        [Comment("The identifier of the user whose action triggered the notification")]
+        public Guid TriggeredById { get; set; }
+
+        [ForeignKey(nameof(TriggeredById))]
+        [Comment("The user whose action triggered the notification")]
+        public virtual ApplicationUser TriggeredBy { get; set; } = null!;
+
+    }
+}
