c567c2b698bcb86e806aa22c5ac7f39b45065e0c
Solenkoff <solenkoff@yahoo.com>
2026-03-10 15:03:51 +0200

    Add BaseComment abstract class

---
+++ TheWindCorner.Data.Models/Entities/Comments/BaseComment.cs
@@ -0,0 +1,47 @@
+﻿namespace TheWindCorner.Data.Models.Entities.Comments
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Microsoft.EntityFrameworkCore;
+
+    using TheWindCorner.Data.Models.User;
+
+    using static TheWindCorner.Common.EntityValidationConstants.Comment;
+    using static TheWindCorner.Common.EntityValidationMessages.Comment;
+
+
+    [Comment("The comment on an entity")]
+    public abstract class BaseComment
+    {
+
+        [Key]
+        [Comment("The identifier of the comment made")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(ContentMaxLength, ErrorMessage = ContentMaxLengthMassage)]
+        [Comment("The full text of the comment")]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        [Comment("The date and time the comment was added")]
+        public DateTime CommentedOn { get; set; }
+
+        [Required]
+        [Comment("If the comment has been edited")]
+        public bool IsEdited { get; set; } = false;
+
+        [Required]
+        [Comment("If the comment has been deleted")]
+        public bool IsDeleted { get; set; } = false;
+
+        [Required]
+        [Comment("The identifier of the user, who made the comment")]
+        public Guid AuthorId { get; set; }
+
+        [ForeignKey(nameof(AuthorId))]
+        [Comment("The user, who made the comment")]
+        public virtual ApplicationUser Author { get; set; } = null!;
+
+    }
+}
