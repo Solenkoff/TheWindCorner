21e6a6bf93625021947ad8bf83612ef4b38f4ced
Solenkoff <solenkoff@yahoo.com>
2026-03-10 15:49:02 +0200

    Alter ItemComment, WantedItemComment, SpotComment, EventComment  to inherit from BaseComment

--- TheWindCorner.Data.Models/Entities/ItemComment.cs
+++ TheWindCorner.Data.Models/Entities/ItemComment.cs
@@ -4,44 +4,12 @@
     using System.ComponentModel.DataAnnotations.Schema;
     using Microsoft.EntityFrameworkCore;
 
-    using TheWindCorner.Data.Models.User;
-
-    using static TheWindCorner.Common.EntityValidationConstants.ItemComment;
-    using static TheWindCorner.Common.EntityValidationMessages.ItemComment;
+    using TheWindCorner.Data.Models.Entities.Comments;
 
 
     [Comment("The comment on an item")]
-    public class ItemComment
+    public class ItemComment : BaseComment
     {
-
-        [Key]
-        [Comment("The identifier of the comment made")]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        [MaxLength(ContentMaxLength, ErrorMessage = ContentMaxLengthMassage)]
-        [Comment("The full text of the comment")]
-        public string Content { get; set; } = null!;
-
-        [Required]
-        [Comment("The date and time the comment was added or edited")]
-        public DateTime CommentedOn { get; set; }
-
-        [Required]
-        [Comment("If the comment has been edited")]
-        public bool IsEdited { get; set; } = false;
-
-        [Required]
-        [Comment("If the comment has been deleted")]
-        public bool IsDeleted { get; set; } = false;
-
-        [Required]
-        [Comment("The identifier of the user, who made the comment")]
-        public Guid AuthorId { get; set; }
-
-        [ForeignKey(nameof(AuthorId))]
-        [Comment("The user, who made the comment")]
-        public virtual ApplicationUser Author { get; set; } = null!;
 
         [Required]
         [Comment("The identifier of the item, that was commented")]
--- TheWindCorner.Data.Models/Entities/WantedItemComment.cs
+++ TheWindCorner.Data.Models/Entities/WantedItemComment.cs
@@ -4,44 +4,12 @@
     using System.ComponentModel.DataAnnotations.Schema;
     using Microsoft.EntityFrameworkCore;
 
-    using TheWindCorner.Data.Models.User;
-
-    using static TheWindCorner.Common.EntityValidationConstants.WantedItemComment;
-    using static TheWindCorner.Common.EntityValidationMessages.WantedItemComment;
+    using TheWindCorner.Data.Models.Entities.Comments;
 
 
     [Comment("The comment on a wanted item")]
-    public class WantedItemComment
+    public class WantedItemComment : BaseComment
     {
-
-        [Key]
-        [Comment("The identifier of the comment made")]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        [MaxLength(ContentMaxLength, ErrorMessage = ContentMaxLengthMassage)]
-        [Comment("The full text of the comment")]
-        public string Content { get; set; } = null!;
-
-        [Required]
-        [Comment("The date and time the comment was added or edited")]
-        public DateTime CommentedOn { get; set; }
-
-        [Required]
-        [Comment("If the comment has been edited")]
-        public bool IsEdited { get; set; } = false;
-
-        [Required]
-        [Comment("If the comment has been deleted")]
-        public bool IsDeleted { get; set; } = false;
-
-        [Required]
-        [Comment("The identifier of the user, who made the comment")]
-        public Guid AuthorId { get; set; }
-
-        [ForeignKey(nameof(AuthorId))]
-        [Comment("The user, who made the comment")]
-        public virtual ApplicationUser Author { get; set; } = null!;
 
         [Required]
         [Comment("The identifier of the wanted item, that was commented")]
