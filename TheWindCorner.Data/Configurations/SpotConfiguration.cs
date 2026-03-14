f3ce0352a18509a61d22b82148e0f7c3410aadbf
Solenkoff <solenkoff@yahoo.com>
2026-03-14 11:19:21 +0200

    Add Spot configuration

---
+++ TheWindCorner.Data/Configurations/SpotConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class SpotConfiguration : IEntityTypeConfiguration<Spot>
+    {
+        public void Configure(EntityTypeBuilder<Spot> builder)
+        {
+            builder.HasOne(s => s.CreatedBy)
+                .WithMany(u => u.AddedSpots)
+                .HasForeignKey(s => s.CreatedById)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasMany(s => s.HomeSpotUsers)
+                .WithOne(u => u.HomeSpot)
+                .HasForeignKey(u => u.HomeSpotId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasIndex(s => s.CreatedById);
+        }
+    }
+}
