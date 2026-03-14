49f9148ad61e37a4aeefb1eb273e80492750ff1c
Solenkoff <solenkoff@yahoo.com>
2026-03-14 15:14:00 +0200

    Add WantedItemComment configuration

---
+++ TheWindCorner.Data/Configurations/WantedItemCommentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class WantedItemCommentConfiguration : IEntityTypeConfiguration<WantedItemComment>
+    {
+        public void Configure(EntityTypeBuilder<WantedItemComment> builder)
+        {
+            builder.HasOne(wic => wic.Author)
+                .WithMany()
+                .HasForeignKey(wic => wic.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(wic => wic.WantedItem)
+                .WithMany(wi => wi.Comments)
+                .HasForeignKey(wic => wic.WantedItemId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasIndex(wic => wic.AuthorId);
+        }
+    }
+}
