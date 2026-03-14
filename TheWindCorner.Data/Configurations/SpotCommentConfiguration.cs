d8ea82ea5564d48a85275a0c3469eccfb52c426b
Solenkoff <solenkoff@yahoo.com>
2026-03-14 15:27:36 +0200

    Add SpotComment configuration

---
+++ TheWindCorner.Data/Configurations/SpotCommentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class SpotCommentConfiguration : IEntityTypeConfiguration<SpotComment>
+    {
+        public void Configure(EntityTypeBuilder<SpotComment> builder)
+        {
+            builder.HasOne(sc => sc.Author)
+                .WithMany()
+                .HasForeignKey(sc => sc.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(sc => sc.Spot)
+                .WithMany(s => s.Comments)
+                .HasForeignKey(sc => sc.SpotId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasIndex(sc => sc.AuthorId);
+        }
+    }
+}
