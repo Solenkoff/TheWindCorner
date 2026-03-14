4ecb26cf17e22282bd8e0e8d35289b57090dba6a
Solenkoff <solenkoff@yahoo.com>
2026-03-14 14:54:34 +0200

    Add ItemComment configuration

---
+++ TheWindCorner.Data/Configurations/ItemCommentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class ItemCommentConfiguration : IEntityTypeConfiguration<ItemComment>
+    {
+        public void Configure(EntityTypeBuilder<ItemComment> builder)
+        {
+            builder.HasOne(ic => ic.Author)
+                .WithMany()
+                .HasForeignKey(ic => ic.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(ic => ic.Item)
+                .WithMany(i => i.Comments)
+                .HasForeignKey(ic => ic.ItemId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasIndex(ic => ic.AuthorId);
+        }
+    }
+}
