81059ccf777e93c1d2952f9c3b07f359222de341
Solenkoff <solenkoff@yahoo.com>
2026-03-14 15:46:17 +0200

    Add EventComment configuration

---
+++ TheWindCorner.Data/Configurations/EventCommentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class EventCommentConfiguration : IEntityTypeConfiguration<EventComment>
+    {
+        public void Configure(EntityTypeBuilder<EventComment> builder)
+        {
+            builder.HasOne(ec => ec.Author)
+                .WithMany()
+                .HasForeignKey(ec => ec.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(ec => ec.Event)
+                .WithMany(e => e.Comments)
+                .HasForeignKey(ec => ec.EventId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasIndex(ec => ec.AuthorId);
+        }
+    }
+}
