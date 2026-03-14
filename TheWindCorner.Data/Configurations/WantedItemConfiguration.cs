89f13a38c8097a14e2e885a8588d142133b38486
Solenkoff <solenkoff@yahoo.com>
2026-03-14 10:56:51 +0200

    Add WantedItem configuration

---
+++ TheWindCorner.Data/Configurations/WantedItemConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class WantedItemConfiguration : IEntityTypeConfiguration<WantedItem>
+    {
+        public void Configure(EntityTypeBuilder<WantedItem> builder)
+        {
+            builder.Property(wi => wi.Category)
+                .HasConversion<string>();
+
+            builder.HasOne(wi => wi.CreatedBy)
+                .WithMany(u => u.WantedItems)
+                .HasForeignKey(wi => wi.CreatedById)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasIndex(wi => wi.CreatedById);
+        }
+    }
+}
