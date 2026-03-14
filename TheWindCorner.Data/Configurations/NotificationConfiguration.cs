de2d57aaf7a3a64c9841a346a12e610d2911b8db
Solenkoff <solenkoff@yahoo.com>
2026-03-14 14:32:50 +0200

    Add Notification configuration

---
+++ TheWindCorner.Data/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,28 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.Property(n => n.EntityType)
+                .HasConversion<string>();
+
+            builder.Property(n => n.ActionType)
+                .HasConversion<string>();
+
+            builder.HasOne(n => n.TriggeredBy)
+                .WithMany()
+                .HasForeignKey(n => n.TriggeredById)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasIndex(n => new { n.IsRead, n.IsHandled });
+
+            builder.HasIndex(n => n.TriggeredById);
+        }
+    }
+}
