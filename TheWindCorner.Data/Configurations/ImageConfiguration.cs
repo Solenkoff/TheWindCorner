c68cbc96b9eff877c6686049266dd9838e5d7309
Solenkoff <solenkoff@yahoo.com>
2026-03-14 13:56:59 +0200

    Add Image configuration

---
+++ TheWindCorner.Data/Configurations/ImageConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class ImageConfiguration : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.Property(i => i.EntityType)
+                .HasConversion<string>();
+
+            builder.Property(i => i.Path)
+                .IsRequired();
+
+            builder.HasIndex(i => new { i.EntityType, i.EntityId, i.DisplayOrder })
+                .IsUnique();
+        }
+    }
+}
