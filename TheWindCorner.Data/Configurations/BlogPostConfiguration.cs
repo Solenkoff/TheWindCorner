de6fdc5de8dd615c965bbff4e26bec88db2559f4
Solenkoff <solenkoff@yahoo.com>
2026-03-14 12:12:22 +0200

    Add BlogPost configuration

---
+++ TheWindCorner.Data/Configurations/BlogPostConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace TheWindCorner.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TheWindCorner.Data.Models.Entities;
+
+    public class BlogPostConfiguration : IEntityTypeConfiguration<BlogPost>
+    {
+        public void Configure(EntityTypeBuilder<BlogPost> builder)
+        {
+            builder.HasOne(bp => bp.CreatedBy)
+                .WithMany(u => u.BlogPosts)
+                .HasForeignKey(bp => bp.CreatedById)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
